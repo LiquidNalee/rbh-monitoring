24f32b006a7f42e49d60020c565fe2c166a0639e
Sami BOUCENNA <sami.boucenna@epita.fr>
2017-07-21 11:18:52 +0200

    Test for database list missmatch

--- rbh_monitoring/rbhMonitor.py
+++ rbh_monitoring/rbhMonitor.py
@@ -147,6 +147,10 @@ def graph():
     else:
         dry_run = False
 
+    if (len(DB_HOST) is not len(DB_USER)) or (len(DB_HOST) is not len(DB_PWD)) or (len(DB_HOST) is not len(DB)):
+        print 'Error: Mismatch in database information list length\nVerify your arguments/conf.file list of databases'
+        exit(1)
+
     logAgeTab = ['modif', 'last_mod', 'acs', 'last_access', 'creat', 'creation_time', 'db', 'last_mdchange']
     timespanTab = [('15min', 900), ('1h', 3600), ('12h', 43200), ('1d', 86400), ('1w', 604800), ('1m', 2592000), ('6m', 15552000), ('1y', 31104000), ('over1y', 0)]
     ite = 0
